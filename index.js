e631a0f080b57effe38c9315e8adc3cf453b59ec
notasimp <164659222+notasimpp@users.noreply.github.com>
2024-03-28 16:46:35 +0545

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+function validateLogin() {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    const errorElement = document.getElementById('login-error');
+
+    if (username === 'grishma' && password === '0918') {
+        // Redirect to index.html upon successful login
+        window.location.href = 'index.html';
+        return false; // Prevent form submission
+    } else {
+        // Invalid credentials, show error message
+        errorElement.textContent = 'Invalid username or password.';
+        return false;
+    }
+}
